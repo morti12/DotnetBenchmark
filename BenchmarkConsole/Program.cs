3b58b38eaa1b4ea588a73030540d11776fa4e2a7
martin.balter <martin.balter@fusonic.net>
2021-09-29 22:42:40 +0200

    Compare ToList vs ToArray

---
+++ BenchmarkConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using ToListToArrayBenchmark;
+using BenchmarkDotNet.Running;
+
+namespace BenchmarkConsole
+{
+    public class Program 
+    { 
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ToListVsToArray>();
+        }
+    }
+}
---
+++ ToListToArrayBenchmark/ToListVsToArray.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToListToArrayBenchmark
+{
+    [MemoryDiagnoser]
+    public class ToListVsToArray
+    {
+        public static IEnumerable<int> TestEnumerable => Enumerable.Range(0, 100);
+
+        [Benchmark]
+        public void ToList() => TestEnumerable.ToList();
+
+        [Benchmark]
+        public void ToArray() => TestEnumerable.ToArray();
+    }
+
+}
