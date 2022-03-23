bbc39f7db699ba6ab86c77c71368bbad6eda2f6c
martin.balter <martin.balter@gmail.com>
2022-03-23 22:28:13 +0100

    Add new Test to check if the memory size an array vs a list changed much

---
+++ ArraySizeListSize/Program.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<ArraySizeListSizeBenchmark>();
+    
+[MemoryDiagnoser]
+public class ArraySizeListSizeBenchmark
+{
+    [Params(10, 100, 1000, 10000)] 
+    public int Size { get; set; }
+    
+    [Benchmark]
+    public int[] ArraySize()
+    {
+        var array = new int[Size];
+
+        for (var i = 0; i < Size; i++)
+        {
+            array[i] = i;
+        }
+
+        return array;
+    }
+    
+    [Benchmark]
+    public List<int> ListSize()
+    {
+        var array = new List<int>(Size);
+
+        for (var i = 0; i < Size; i++)
+        {
+            array.Add(i);
+        }
+
+        return array;
+    }
+    
+    [Benchmark]
+    public List<int> ListWithoutSize()
+    {
+        var array = new List<int>();
+
+        for (var i = 0; i < Size; i++)
+        {
+            array.Add(i);
+        }
+
+        return array;
+    }
+}
