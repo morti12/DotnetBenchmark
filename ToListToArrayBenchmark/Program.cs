440a1403b1c0d7243f272c9d09c9ddac5ef3d239
martin.balter <martin.balter@gmail.com>
2022-03-23 22:28:54 +0100

    Improved the ToListToArray Benchmark with more parameters

--- ToListToArrayBenchmark/Program.cs
+++ ToListToArrayBenchmark/Program.cs
@@ -1,3 +1,20 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 BenchmarkRunner.Run<ToListVsToArray>();
+
+[MemoryDiagnoser]
+public class ToListVsToArray
+{
+
+    [Params(10, 100, 1000, 10000)]
+    public int Size { get; set; }
+    
+    public IEnumerable<int> TestEnumerable => Enumerable.Range(0, Size);
+
+    [Benchmark]
+    public List<int> ToList() => TestEnumerable.ToList();
+
+    [Benchmark]
+    public int[] ToArray() => TestEnumerable.ToArray();
+}
