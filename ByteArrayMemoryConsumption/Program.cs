18925ffe65e62520a6e5ea44bea30bb9a678ca32
Martin Balter <martin.balter@liebherr.com>
2022-04-21 17:26:20 +0200

    Check the memory consumption when ToArray is used in comparison with ReadOnlyMemory

---
+++ ByteArrayMemoryConsumption/Program.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<ByteArrayMemoryTest>();
+
+[MemoryDiagnoser]
+public class ByteArrayMemoryTest
+{
+    private static readonly byte[] Bytes = new byte[100];
+
+    [Benchmark]
+    public byte ToArrayGetLast()
+    {
+        return Bytes.ToArray()[^1];
+    }
+
+    [Benchmark]
+    public byte ToReadonlyMemoryGetLast()
+    {
+        return new ReadOnlyMemory<byte>(Bytes).Span[^1];
+    }
+}
