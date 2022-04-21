5f7fe62d52192314291e2a61ab80cdfe0554a954
Martin Balter <martin.balter@liebherr.com>
2022-04-21 16:29:19 +0200

    Add guard clause tests

---
+++ GuardClausesTest/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Ardalis.GuardClauses;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Dawn;
+using EnsureThat;
+
+
+BenchmarkRunner.Run<GuardTests>();
+
+
+[MemoryDiagnoser()]
+public class GuardTests
+{
+    private const string ValueToGuardAgainst = "HelloWorld";
+
+    [Benchmark]
+    public string ArdalisGuardClauses()
+    {
+        return Ardalis.GuardClauses.Guard.Against.NullOrWhiteSpace(ValueToGuardAgainst, nameof(ValueToGuardAgainst));
+    }
+    
+    [Benchmark]
+    public string DawnGuardClauses()
+    {
+        return Dawn.Guard.Argument(ValueToGuardAgainst, nameof(ValueToGuardAgainst)).NotNull().NotWhiteSpace();
+    }
+    
+    [Benchmark]
+    public string EnsureThatClauses()
+    {
+        EnsureThat.Ensure.That(ValueToGuardAgainst, nameof(ValueToGuardAgainst)).IsNotNullOrWhiteSpace();
+        return ValueToGuardAgainst;
+    }
+    
+    [Benchmark]
+    public string EnsureStringClauses()
+    {
+        return EnsureThat.Ensure.String.IsNotNullOrWhiteSpace(ValueToGuardAgainst, nameof(ValueToGuardAgainst));
+    }
+    
+    [Benchmark]
+    public string GuardNetClauses()
+    {
+        GuardNet.Guard.NotNullOrWhitespace(ValueToGuardAgainst, nameof(ValueToGuardAgainst));
+        return ValueToGuardAgainst;
+    }
+}
