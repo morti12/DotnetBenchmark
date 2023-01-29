ffe385ac66521bc71e0d98d2f2047f8c1c2a3f5e
Martin Balter <martin.balter@gmail.com>
2023-01-29 22:49:02 +0100

    Improved Comparison tests

--- NullComparison/Program.cs
+++ NullComparison/Program.cs
@@ -5,52 +5,162 @@ BenchmarkRunner.Run<NullComparison>();
 
 public class NullComparison
 {
+    public static string? stringValue;
+    public static bool boolValue;
+    public static Person? nullPerson;
+    public static Person? objectPerson;
+
+    [GlobalSetup]
+    public static void InitalizeValues()
+    {
+        stringValue = "HelloWorld";
+        boolValue = true;
+        nullPerson = null;
+        objectPerson = new Person("Hello", "World");
+    }
+
+    /// <summary>
+    /// <code>
+    /// return (object)nullPerson == null;
+    /// </code>
+    /// </summary>
     [Benchmark]
     public bool IsNullTrue()
     {
-        Person? person = null;
-
-        return person is null;
+        return nullPerson is null;
     }
-    
+
+    /// <summary>
+    /// Generates:
+    /// <code>
+    /// return (object)nullPerson != null;
+    /// </code>
+    /// </summary>
     [Benchmark]
     public bool IsNullFalse()
     {
-        Person? person = null;
-
-        return person is not null;
+        return nullPerson is not null;
     }
-    
+
+    /// <summary>
+    /// Generates:
+    /// <code>
+    /// return nullPerson == null;
+    /// </code>
+    /// </summary>
     [Benchmark]
     public bool NullEqualsTrue()
     {
-        Person? person = null;
-
-        return person == null;
+        return nullPerson == null;
     }
-    
+
+    /// <summary>
+    /// Generates:
+    /// <code>
+    /// return nullPerson != null;
+    /// </code>
+    /// </summary>
     [Benchmark]
     public bool NullEqualsFalse()
     {
-        Person? person = null;
-
-        return person != null;
+        return nullPerson != null;
     }
-    
+
+    /// <summary>
+    /// Generates:
+    /// <code>
+    /// return (object)nullPerson == null;
+    /// </code>
+    /// </summary>
     [Benchmark]
     public bool NullReferenceEqualsTrue()
     {
-        Person? person = null;
-
-        return ReferenceEquals(null, person);
+        return ReferenceEquals(null, nullPerson);
     }
-    
+
+    /// <summary>
+    /// Generates:
+    /// <code>
+    /// return (object)nullPerson == null;
+    /// </code>
+    /// </summary>
     [Benchmark]
     public bool NullReferenceEqualsFalse()
     {
-        Person? person = null;
+        return ReferenceEquals(nullPerson, null);
+    }
 
-        return ReferenceEquals(person, null);
+    /// <summary>
+    /// Generates:
+    /// <code>
+    /// return boolValue;
+    /// </code>
+    /// </summary>
+    [Benchmark]
+    public bool BooleanIsTrue()
+    {
+        return boolValue is true;
+    }
+
+    /// <summary>
+    /// Generates:
+    /// <code>
+    /// return !boolValue;
+    /// </code>
+    /// </summary>
+    [Benchmark]
+    public bool BooleanIsFalse()
+    {
+        return boolValue is false;
+    }
+
+    /// <summary>
+    /// Generates:
+    /// <code>
+    /// return stringValue == null;
+    /// </code>
+    /// </summary>
+    [Benchmark]
+    public bool StringIsNullFalse()
+    {
+        return stringValue is null;
+    }
+
+    /// <summary>
+    /// Generates:
+    /// <code>
+    /// return stringValue != null;
+    /// </code>
+    /// </summary>
+    [Benchmark]
+    public bool StringIsNotNullFalse()
+    {
+        return stringValue is not null;
+    }
+
+    /// <summary>
+    /// Generates:
+    /// <code>
+    /// return (object)objectPerson != null;
+    /// </code>
+    /// </summary>
+    [Benchmark]
+    public bool PersonIsObject()
+    {
+        return objectPerson is { };
+    }
+
+    /// <summary>
+    /// Generates:
+    /// <code>
+    /// Person person = objectPerson;
+    /// return (object)person != null && person.Forename == "Hello";
+    /// </code>
+    /// </summary>
+    [Benchmark]
+    public bool PersonCompareValues()
+    {
+        return objectPerson is { Forename: "Hello" };
     }
 }
 
